44098c3705a7124f71d91d14740409e911852a45
Alexander Kallaway <kallaway@me.com>
2018-02-02 23:41:26 -0500

    add questions asking, later figure out locking

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ var correctCount int
 func main() {
 	fmt.Println("Welcome")
 	// first, see if there is a flag that specified name of the file.
-
 	// if none: use and read problems.csv
 
 	problems, err := ioutil.ReadFile("quiz-data/problems.csv")
@@ -23,10 +22,7 @@ func main() {
 	problemsString := string(problems)
 
 	qaPairs := strings.Split(problemsString, "\n")
-
-	for i, v := range qaPairs {
-		fmt.Printf("%v %v\n", i, v)
-	}
+	questionsCount := len(qaPairs)
 
 	var quizDataSlice [][]string
 
@@ -36,5 +32,21 @@ func main() {
 		quizDataSlice = append(quizDataSlice, qaPair)
 	}
 
+	// fmt.Printf("The length of the quizDataSlice is %v", len(quizDataSlice))
+
 	// ask questions and if they are correct - update score. in the end, post score
+	for i := range quizDataSlice {
+		qa := quizDataSlice[i]
+		q := qa[0]
+		a := qa[1]
+		fmt.Printf("Q%v: %s\n", (i + 1), q)
+		var userResponse string
+		fmt.Scanln(userResponse)
+
+		if a == userResponse {
+			correctCount++
+		}
+	}
+	// post score
+	fmt.Printf("Game over. You've answered %v out of %v questions correctly.", correctCount, questionsCount)
 }
